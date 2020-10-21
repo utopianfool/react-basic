d6ff3ee7628f8ad339ea708ee022a565cd851ff8
Phil <phil@evolvedigital.co.uk>
2020-10-21 15:27:12 +0100

    Updates

---
+++ precache-manifest.9bc08c0c7f9ff2819ba330356fe97d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508e95d9036f086196ecb840b7e4bd57",
+    "url": "/react-basic/index.html"
+  },
+  {
+    "revision": "f0ad1c08c8df35b06d25",
+    "url": "/react-basic/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "1fcd138d4543c7bc167c",
+    "url": "/react-basic/static/js/2.22a8f1ec.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-basic/static/js/2.22a8f1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ad1c08c8df35b06d25",
+    "url": "/react-basic/static/js/main.ffb62cb7.chunk.js"
+  },
+  {
+    "revision": "ee66c33e19544a9b83ef",
+    "url": "/react-basic/static/js/runtime-main.cd9a73f4.js"
+  }
+]);
